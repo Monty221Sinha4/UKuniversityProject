fb815b511877ebbe8dfc654846285b3632111065
Monty Sinha <monty@vayaconnect.com>
2018-03-15 23:46:54 +0000

    Add files via upload

---
+++ CountyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityProject.Class_Libary;
+using DataBusinessLayer;
+namespace UniversityProject.Controllers
+{
+    public class CountyController : Controller
+    {
+        // GET: County
+        public ActionResult CountyPage()
+        {
+            DataBusinessClass databusinessLayer = new DataBusinessClass();
+            List<County> count = databusinessLayer.count.ToList();
+            
+            return View(count);
+        }
+    }
+}
